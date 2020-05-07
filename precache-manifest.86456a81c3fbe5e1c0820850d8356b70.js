5cb30e4ae155a8c863293cca5d3e3d44b21654fa
Eduard Buchenko <buchenko@gmail.com>
2020-05-07 23:44:51 +0300

    create gh-pages

---
+++ precache-manifest.86456a81c3fbe5e1c0820850d8356b70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "55b1a0db1bf4d0e67b5c",
+    "url": "/reactwarriors-movies-db/static/css/main.4cd2eec2.chunk.css"
+  },
+  {
+    "revision": "55b1a0db1bf4d0e67b5c",
+    "url": "/reactwarriors-movies-db/static/js/main.0fcf1903.chunk.js"
+  },
+  {
+    "revision": "b58c94d7bd5850bc7306",
+    "url": "/reactwarriors-movies-db/static/js/runtime~main.7058b3c3.js"
+  },
+  {
+    "revision": "d637c3857a1e0872f50e",
+    "url": "/reactwarriors-movies-db/static/css/2.0cbadeac.chunk.css"
+  },
+  {
+    "revision": "d637c3857a1e0872f50e",
+    "url": "/reactwarriors-movies-db/static/js/2.89306c1e.chunk.js"
+  },
+  {
+    "revision": "b1eec7409c07dce06c89b6881bcbd7f5",
+    "url": "/reactwarriors-movies-db/index.html"
+  }
+];
